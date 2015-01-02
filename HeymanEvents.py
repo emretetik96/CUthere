7eae1f4fda2890974fa7bd656a672e2924a193de
Emre Tetik <emretetik@Emres-MacBook-Pro.local>
2015-01-02 01:42:19 -0500

    Improved HeymanEvents

--- HeymanEvents.py
+++ HeymanEvents.py
@@ -10,7 +10,16 @@ def get_event_page_urls():
 	resultEvents = [a.attrs.get('href') for a in soup.select('div.topic a[href*="/events"]') ]
 	return resultEvents
 
-def get_event_data(event_page_urls):
-	
+def get_event_data(event_page_url):
+	event_data = {}
+	response = requests.get(event_page_url + root_url)
+	soup = bs4.BeautifulSoup(response.text)
+	event_data['title'] = soup.select('itemprop="name"').get_text()
+	return event_data
+
+def show_event_info():
+	event_page_urls = get_event_page_urls()
+	for event_page_url in event_page_urls:
+		print get_event_data(event_page_url)
 
 print get_event_page_urls()
