33547fa187eb967455aac60326b814450250c631
Emre Tetik <emretetik@Emres-MacBook-Pro.local>
2015-01-02 02:18:16 -0500

    titles successfully scraped

--- HeymanEvents.py
+++ HeymanEvents.py
@@ -12,14 +12,14 @@ def get_event_page_urls():
 
 def get_event_data(event_page_url):
 	event_data = {}
-	response = requests.get(event_page_url) ## + root_url)
+	response = requests.get(event_page_url)
 	soup = bs4.BeautifulSoup(response.text)
-	event_data['title'] = soup.select('span[itemprop="name"]') ##.get_text()
+	event_data['title'] = soup.select('div.heading span[itemprop="name"]')
 	return event_data
 
 def show_event_info():
 	event_page_urls = get_event_page_urls()
 	for event_page_url in event_page_urls:
 		print get_event_data(event_page_url)
-
+			
 print show_event_info()
