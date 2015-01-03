a6afe3f52b7a7c40db8603a41b4636cfabe9d191
Emre Tetik <emretetik@Emres-MacBook-Pro.local>
2015-01-03 01:18:00 -0500

    unicode issue sorted out

--- HeymanEvents.py
+++ HeymanEvents.py
@@ -15,11 +15,18 @@ def get_event_data(event_page_url):
 	response = requests.get(event_page_url)
 	soup = bs4.BeautifulSoup(response.text)
 	event_data['title'] = soup.select('div.heading span[itemprop="name"]')[0].get_text()
-	return event_data.values()
+	for key in event_data:
+		##print isinstance(event_data[key], unicode)
+		return event_data[key]
+		##event_data[key].encode('utf_8', 'ignore')
+		##event_data[key] = event_data[key].decode('utf_8')		
+	##return event_data.values()
 
 def show_event_info():
 	event_page_urls = get_event_page_urls()
 	for event_page_url in event_page_urls:
-		print get_event_data(event_page_url)
-			
-print show_event_info()
+		print(get_event_data(event_page_url))
+
+##for testing purposes
+print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')			
+print(show_event_info())
