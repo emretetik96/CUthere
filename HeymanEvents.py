aba54f8fabe3522fe4ecda34d6fa5e608b4c73da
Emre Tetik <emretetik@Emres-MacBook-Pro.local>
2015-01-03 14:20:40 -0500

    documented

--- HeymanEvents.py
+++ HeymanEvents.py
@@ -5,12 +5,15 @@ ROOT_URL = 'http://heymancenter.org/events'
 INDEX_URL = ROOT_URL + '/semester/spring-2015/'
 ORGANIZATION_TITLE = 'Heyman Center for the Humanities'
 
+#scrapes the URLs of single event pages from the INDEX_URL page
 def get_event_page_urls():
 	response = requests.get(INDEX_URL)
 	soup = bs4.BeautifulSoup(response.text)
 	resultEvents = [a.attrs.get('href') for a in soup.select('div.topic a[href*="/events"]') ]
 	return resultEvents
 
+#returns the data of the event whose page URL is passed as a parameter;
+#returns the data in the form of a dictionary
 def get_event_data(event_page_url):
 	event_data = {}
 	response = requests.get(event_page_url)
@@ -18,11 +21,14 @@ def get_event_data(event_page_url):
 	event_data['event_title'] = soup.select('div.heading span[itemprop="name"]')[0].get_text()
 	event_data['organization_title'] = ORGANIZATION_TITLE
 	event_data['location'] = soup.select('span[itemprop="location"]')[0].get_text()
-
+	#reading multiple paragraphs into a single dictionary value matched to the
+	#'event_description' key
 	event_data['event_description'] = ''
 	for paragraph in soup.select('div[itemprop="description"] p'):
 		event_data['event_description'] += paragraph.get_text()
-
+	#separates date and time as two different values and places them 
+	#in their respective keys; if the page doesn't provide a time, then a
+	#'time' key is not made
 	dateTimeTogether = soup.select('em.date')[0].get_text()
 	if (dateTimeTogether.endswith('am') or dateTimeTogether.endswith('pm')):
 		timeIndex = dateTimeTogether.find(':') - 2
@@ -33,7 +39,7 @@ def get_event_data(event_page_url):
 
 	return event_data
 	
-
+#prints the information of each event on the INDEX_URL page 
 def show_event_info():
 	event_page_urls = get_event_page_urls()
 	for event_page_url in event_page_urls:
