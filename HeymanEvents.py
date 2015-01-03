9894408e29848781c42e1563741a5c9ed5b1f826
Emre Tetik <emretetik@Emres-MacBook-Pro.local>
2015-01-03 13:19:05 -0500

    new attributes for each event added

--- HeymanEvents.py
+++ HeymanEvents.py
@@ -15,16 +15,25 @@ def get_event_data(event_page_url):
 	event_data = {}
 	response = requests.get(event_page_url)
 	soup = bs4.BeautifulSoup(response.text)
-	event_data['title'] = soup.select('div.heading span[itemprop="name"]')[0].get_text()
+	event_data['event_title'] = soup.select('div.heading span[itemprop="name"]')[0].get_text()
 	event_data['organization_title'] = ORGANIZATION_TITLE
-	for key in event_data:
-		return event_data
+	event_data['location'] = soup.select('span[itemprop="location"]')[0].get_text()
+	dateTimeTogether = soup.select('em.date')[0].get_text()
+	if (dateTimeTogether.endswith('am') or dateTimeTogether.endswith('pm')):
+		timeIndex = dateTimeTogether.find(':') - 2
+		event_data['date'] = dateTimeTogether[:timeIndex]
+		event_data['time'] = dateTimeTogether[timeIndex:]
+	else:
+		event_data['date'] = dateTimeTogether
+	return event_data
+	
 
 def show_event_info():
 	event_page_urls = get_event_page_urls()
 	for event_page_url in event_page_urls:
 		print(get_event_data(event_page_url))
 
+	
 ##for testing purposes
 print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')			
 show_event_info()
