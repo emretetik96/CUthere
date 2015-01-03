348509c3980e18d0012e1547d56b317d1eb3970d
Emre Tetik <emretetik@Emres-MacBook-Pro.local>
2015-01-03 01:22:59 -0500

    cleaned up

--- HeymanEvents.py
+++ HeymanEvents.py
@@ -16,11 +16,7 @@ def get_event_data(event_page_url):
 	soup = bs4.BeautifulSoup(response.text)
 	event_data['title'] = soup.select('div.heading span[itemprop="name"]')[0].get_text()
 	for key in event_data:
-		##print isinstance(event_data[key], unicode)
 		return event_data[key]
-		##event_data[key].encode('utf_8', 'ignore')
-		##event_data[key] = event_data[key].decode('utf_8')		
-	##return event_data.values()
 
 def show_event_info():
 	event_page_urls = get_event_page_urls()
@@ -29,4 +25,4 @@ def show_event_info():
 
 ##for testing purposes
 print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')			
-print(show_event_info())
+show_event_info()
